d71e45362278d24b83cbf5363ec63f6e5217d63d
Rui Liu <rui.liu@gmx.com>
2018-01-22 12:11:09 -0600

    finish

--- hw1/student_code.h
+++ hw1/student_code.h
@@ -3,19 +3,29 @@
 //required libraries
 #include <vector>
 
-
+using namespace std;
 // This function should return your name.
 // The name should match your name in Canvas
 
 void GetStudentName(std::string& your_name)
 {
-   your_name.assign("Yury Makarychev");
+   your_name.assign("Rui Liu");
 }
 
 //This is the function you need to implement.
 int FindMaximumProfit(std::vector<int> w)
 {
-    //write your solution here
+    int n = w.size();
+    int sw[n];
 
-    return /* your answer */;
+    sw[0] = w[0];
+    sw[1] = w[0] + w[1];
+    sw[2] = max(max(w[1] + w[2], w[0] + w[2]), sw[1]);
+    for (int i = 3; i < n; ++i) {
+        sw[i] = max(max(w[i] + w[i-1] + sw[i-3], sw[i-2] + w[i]), sw[i-1]);
+    }
+
+    return sw[n-1];
 }
+
+
